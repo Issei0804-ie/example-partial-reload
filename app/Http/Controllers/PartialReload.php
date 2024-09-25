ced65556728925071e3dfebe2a9d2f371a011e95
issei <issei0804@outlook.com>
2024-09-25 20:20:53 +0900

    example partial reload

--- app/Http/Controllers/PartialReload.php
+++ app/Http/Controllers/PartialReload.php
@@ -2,15 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Http\Request;
+use Illuminate\Support\Sleep;
 
 class PartialReload extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request)
     {
-        return inertia('PartialReload', []);
+        $number = $request->input('num', 0);
+
+        return inertia('PartialReload', [
+            'slowExecution' => fn() => $this->verrrrrrrrrySlowExecution(),
+            'num' => fn() => $number + 1,
+        ]);
+    }
+
+    private function verrrrrrrrrySlowExecution(): string
+    {
+        Sleep::sleep(4);
+        return CarbonImmutable::now()->toISOString();
     }
 }
--- resources/js/Pages/PartialReload.vue
+++ resources/js/Pages/PartialReload.vue
@@ -1,10 +1,52 @@
 <script setup lang="ts">
+import {router} from "@inertiajs/vue3";
 
+const props = defineProps({
+    'slowExecution':{
+       type: String,
+    },
+    'num': {
+        type: Number,
+    }
+});
+
+const requestUsingPartialReload= () => {
+    router.reload({
+        only: ['num'],
+        data: {
+            'num':props.num
+        }
+    });
+};
+
+const fullPageReloadRequest = () => {
+    router.reload({
+        data: {
+            'num':props.num
+        }
+    });
+}
 </script>
 
 <template>
-  <div>hogeeee</div>
-</template>
+    <div>
+        <h1>Example Partial Reload</h1>
 
-<style scoped>
-</style>
+        <p>slow execution message is the below</p>
+        <p> {{ slowExecution }}</p>
+
+        <p>number is the below</p>
+        <p> {{ num }} </p>
+
+        <button
+            @click="requestUsingPartialReload"
+        >
+            Using partial reload
+        </button>
+        <button
+            @click="fullPageReloadRequest"
+        >
+            full page reload
+        </button>
+    </div>
+</template>
